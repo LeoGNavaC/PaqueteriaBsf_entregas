e1526ac67c337a83eaaf0ee45fac05c9521bcc3a
Leonardo Gysaell Nava Cortes(JHQGR14) <leonava988@gmail.com>
2024-11-07 10:17:47 -0600

    Modificacion linea 7 - index.php(modificacion para que cuando ingrese rediriga a la pagina datos_bsf.php) Modificacion linea 24,25 - barra_lateral.php(Se eliminaron los datos generales de ambos)

--- barra_lateral.php
+++ barra_lateral.php
@@ -21,9 +21,7 @@
 	<body>
 		<div class="BarraLateral">
 			<ul><!--Se encuentran los links a las demas paginas-->
-			<li><a href="productos_tabla.php">• Datos Generales (Paqueteria)</a></li><!--************se realizo modificacion-->
 				<li><a href="datos_bsf.php">• Datos de entrega (Paqueteria)</a></li>
-				<li><a href="productos_tabla_correspondencia.php">• Datos Generales (correspondencia)</a></li>		
 				<li><a href="datos_bsf_correspondencia.php">• Datos de Entrega (correspondencia)</a></li>		
 				<li><a href="cerrar_sesion.php" >• Cerrar sesión</a></li>
 			</ul>
